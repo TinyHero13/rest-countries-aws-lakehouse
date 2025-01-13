069179257bd8b8ade770b9d9a21a0399ec613288
TinyHero13 <yasmimcharles@protonmail.com>
2025-01-13 08:58:34 -0300

    feat: create bucket s3

--- src/main.py
+++ src/main.py
@@ -1,7 +1,28 @@
 import requests
+import boto3
 
+# AWS
+region = 'us-east-1'
+bucket_name = 'rest-countries-data-lake'
+
+# Clients
+s3_client = boto3.client('s3', region_name=region)
+
+# Rest Countries URL
 API_URL = 'https://restcountries.com/v3.1/lang/portuguese'
 
+def create_s3_bucket():
+    try:
+        if region == 'us-east-1':
+            s3_client.create_bucket(Bucket=bucket_name)
+        else:
+            s3_client.create_bucket(
+                Bucket=bucket_name,
+                CreateBucketConfiguration={'LocationConstraint': region}
+            )
+        print(f'Bucket {bucket_name} criado com sucesso')
+    except Exception as e:
+        print(f'Erro ao criar o {bucket_name}: {e}')
 def fetch_api():
     try:
         response = requests.get(API_URL)
@@ -14,6 +35,7 @@ def fetch_api():
 
 
 def main():
+    create_s3_bucket()
     fetch_api()
 
 main()
