b658dbf10c725432f7762cc0e4a3960b8bde0b31
TinyHero13 <yasmimcharles@protonmail.com>
2025-01-13 09:03:26 -0300

    feat: create glue db

--- src/main.py
+++ src/main.py
@@ -4,9 +4,11 @@ import boto3
 # AWS
 region = 'us-east-1'
 bucket_name = 'rest-countries-data-lake'
+glue_db_name = 'glue-countries-lake'
 
 # Clients
 s3_client = boto3.client('s3', region_name=region)
+glue_client = boto3.client('glue', region_name=region)
 
 # Rest Countries URL
 API_URL = 'https://restcountries.com/v3.1/lang/portuguese'
@@ -23,6 +25,19 @@ def create_s3_bucket():
         print(f'Bucket {bucket_name} criado com sucesso')
     except Exception as e:
         print(f'Erro ao criar o {bucket_name}: {e}')
+
+def create_glue_database():
+    try:
+        glue_client.create_database(
+            DatabaseInput = {
+                'Name': glue_db_name,
+                'Description': 'Glue database for Steam analytics'
+            }
+        )
+        print(f'O banco de dados {glue_db_name} do glue foi criado com sucesso')
+    except Exception as e:
+        print(f'Erro em criar o banco de dados do glue: {e}')
+
 def fetch_api():
     try:
         response = requests.get(API_URL)
@@ -36,6 +51,8 @@ def fetch_api():
 
 def main():
     create_s3_bucket()
+    create_glue_database()
+
     fetch_api()
 
 main()
